ae4ca1b1bf3c0844f0104f1e482de0e98d64ae1e
Bhagath M <33441778+bhagath555@users.noreply.github.com>
2023-04-12 14:47:14 +0200

    [Unit-test] Add Good run of CoreNURBS module

--- unittest/Caribou/Topology/test_CoreNURBS.cpp
+++ unittest/Caribou/Topology/test_CoreNURBS.cpp
@@ -2,10 +2,10 @@
 #include <gtest/gtest.h>
 #include "topology_test.h"
 #include <Caribou/Topology/IO/CoreNURBS.h>
-#include <Caribou/Topology/IO/CoreNURBS_1.h>
-#include <Caribou/Topology/SplinePatch.h>
-#include <Caribou/Topology/IO/NURBSReader.h>
-#include <Caribou/Topology/IO/trial_io.h>
+//#include <Caribou/Topology/IO/CoreNURBS_1.h>
+//#include <Caribou/Topology/SplinePatch.h>
+//#include <Caribou/Topology/IO/NURBSReader.h>
+//#include <Caribou/Topology/IO/trial_io.h>
 
 TEST(CoreNURBS, para_topo) {
     // Testing para_topo data structure.
@@ -37,68 +37,108 @@ TEST(CoreNURBS, para_topo) {
     element_connectivity << 0, 1, 2, 4, 5, 6, 8, 9, 10,
                             1, 2, 3, 5, 6, 7, 9, 10, 11;
 
-    para_topo topo_info(element_ranges, element_connectivity);
+    utils::para_topo topo_info(element_ranges, element_connectivity);
     EXPECT_MATRIX_EQUAL(topo_info.get_elrange(), element_ranges);
     EXPECT_MATRIX_EQUAL(topo_info.get_elconn(), element_connectivity);
 }
 
-TEST(CoreNURBS, reader) {
+TEST(CoreNURBS, reader_knot) {
     using namespace caribou;
     using namespace caribou::topology::io;
     coreNurbs nurbs_patch;
-    nurbs_patch.SetFileName(executable_directory_path + "/meshes/splines/3D_spline_surface.txt");
+    nurbs_patch.SetFileName(executable_directory_path + "/meshes/splines/knot_test_geo.txt");
     nurbs_patch.Update();
 
     EXPECT_EQ(nurbs_patch.GetP(), 2);
     EXPECT_EQ(nurbs_patch.GetQ(), 2);
-    EXPECT_EQ(nurbs_patch.get_no_pnts_u(), 4);
+    EXPECT_EQ(nurbs_patch.get_no_pnts_u(), 5);
     EXPECT_EQ(nurbs_patch.get_no_pnts_v(), 3);
-    EXPECT_EQ(nurbs_patch.GetNumberOfPoints(), 12);
-    EXPECT_EQ(nurbs_patch.get_no_elems_u(), 2);
+    EXPECT_EQ(nurbs_patch.GetNumberOfPoints(), 15);
+    EXPECT_EQ(nurbs_patch.get_no_elems_u(), 3);
     EXPECT_EQ(nurbs_patch.get_no_elems_v(), 1);
+    EXPECT_EQ(nurbs_patch.GetNumberOfElements(), 3);
 
-    Double_Vector knot_u(7), knot_v(6);
-    knot_u << 0, 0, 0, 0.5, 1, 1, 1;
+    // Knot vector
+    Double_Vector knot_u(8), knot_v(6);
+    knot_u << 0, 0, 0, 0.33, 0.66, 1, 1, 1;
     knot_v << 0, 0, 0, 1, 1, 1;
-
     EXPECT_MATRIX_EQUAL(nurbs_patch.get_knot_u(), knot_u);
     EXPECT_MATRIX_EQUAL(nurbs_patch.get_knot_v(), knot_v);
 
+    // Extraction Matrix
 
 }
 
-//TEST(CoreNURBS, tt1) {
-//    using namespace caribou;
-//    using namespace caribou::topology::io;
-//    trial t;
-//    t.SetFileName(executable_directory_path + "/meshes/splines/test_io.txt");
-//    t.Update();
-//    EXPECT_EQ(2, 2);
-//    EXPECT_EQ(t.GetPdim(), 10);
-//    EXPECT_EQ(t.GetRealDim(), 11);
-//    Int_Vector b(2);
-//    b << 10, 11;
-//    EXPECT_MATRIX_EQUAL(t.get_vec(), b);
-//    std::cout << "Vector " << b << " \n";
-//}
 
+TEST(CoreNURBS, reader_rectangle) {
+    using namespace caribou;
+    using namespace caribou::topology::io;
+    coreNurbs nurbs_patch;
+    nurbs_patch.SetFileName(executable_directory_path + "/meshes/splines/xy_rectangle.txt");
+    nurbs_patch.Update();
 
-//TEST(CoreNURBS, reader_1) {
-//    using namespace caribou;
-//    using namespace caribou::topology::io;
-//    coreNurbs_1 nurbs_patch;
-//    nurbs_patch.SetFileName(executable_directory_path + "/meshes/splines/3D_spline_surface.txt");
-//    nurbs_patch.Update();
+    EXPECT_EQ(nurbs_patch.GetP(), 2);
+    EXPECT_EQ(nurbs_patch.GetQ(), 2);
 
-//    EXPECT_EQ(nurbs_patch.GetP(), 2);
-//    EXPECT_EQ(nurbs_patch.GetQ(), 2);
-//    EXPECT_EQ(nurbs_patch.get_no_pnts_u(), 4);
-//    EXPECT_EQ(nurbs_patch.get_no_pnts_v(), 3);
-//    EXPECT_EQ(nurbs_patch.GetNumberOfPoints(), 12);
-//    Double_Vector knot_u(7), knot_v(6);
-//    knot_u << 0, 0, 0, 0.5, 1, 1, 1;
-//    knot_v << 0, 0, 0, 1, 1, 1;
+    // Points
+    Double_Matrix pnts(9,2);
+    pnts << 0, 0,
+            1, 0,
+            2, 0,
+            0, 1,
+            1, 1,
+            2, 1,
+            0, 2,
+            1, 2,
+            2, 2;
+    EXPECT_MATRIX_EQUAL(nurbs_patch.GetPoints(), pnts);
+    EXPECT_MATRIX_EQUAL(nurbs_patch.GetPoint(4), pnts.row(4));
+    EXPECT_MATRIX_EQUAL(nurbs_patch.GetPoint(6), pnts.row(6));
+    EXPECT_MATRIX_EQUAL(nurbs_patch.GetPoint(8), pnts.row(8));
 
-//    EXPECT_MATRIX_EQUAL(nurbs_patch.get_knot_u(), knot_u);
-//    EXPECT_MATRIX_EQUAL(nurbs_patch.get_knot_v(), knot_v);
-//}
+}
+
+TEST(CoreNURBS, quarter_cylinder) {
+    using namespace caribou;
+    using namespace caribou::topology::io;
+    coreNurbs nurbs_patch;
+    nurbs_patch.SetFileName(executable_directory_path + "/meshes/splines/quarter_cylinder.txt");
+    nurbs_patch.Update();
+
+    EXPECT_EQ(nurbs_patch.GetP(), 2);
+    EXPECT_EQ(nurbs_patch.GetQ(), 2);
+
+    // Weights
+    Double_Vector wgts(12);
+    wgts << 1.0000,0.853553390593274,0.853553390593274,1.0000, 1.0000,0.853553390593274,0.853553390593274,1.0000, 1.0000,0.853553390593274,0.853553390593274,1.0000 ;
+
+    EXPECT_MATRIX_EQUAL(nurbs_patch.GetWeights(), wgts);
+    EXPECT_EQ(nurbs_patch.GetWeight(0), wgts(0));
+    EXPECT_EQ(nurbs_patch.GetWeight(1), wgts(1));
+
+    // Indices - Unsigned integer type
+    USInt_Matrix indices(nurbs_patch.GetNumberOfElements(), nurbs_patch.GetNumberOfElementPoints());
+    indices << 0, 1, 2, 4, 5, 6, 8, 9, 10,
+               1, 2, 3, 5, 6, 7, 9, 10, 11;
+    EXPECT_MATRIX_EQUAL(nurbs_patch.GetIndices(), indices);
+
+    // KnotRanges
+    Double_Matrix  knotranges(nurbs_patch.GetNumberOfElements(), 4);
+    knotranges << 0, 0, 0.5, 1,
+                  0.5, 0, 1, 1;
+    EXPECT_MATRIX_EQUAL(nurbs_patch.GetKnotRanges(), knotranges);
+
+    Double_Matrix ext1(9,9);
+    ext1 << 1,  0,  0,  0,  0,  0,  0,  0,  0,
+            0,  1,  0.5,0,  0,  0,  0,  0,  0,
+            0,  0,  0.5,0,  0,  0,  0,  0,  0,
+            0,  0,  0,  1,  0,  0,  0,  0,  0,
+            0,  0,  0,  0,  1,  0.5,0,  0,  0,
+            0,  0,  0,  0,  0,  0.5,0,  0,  0,
+            0,  0,  0,  0,  0,  0,  1,  0,  0,
+            0,  0,  0,  0,  0,  0,  0,  1,  0.5,
+            0,  0,  0,  0,  0,  0,  0,  0,  0.5;
+    EXPECT_MATRIX_EQUAL(nurbs_patch.GetExtraction(0), ext1);
+
+}
+
