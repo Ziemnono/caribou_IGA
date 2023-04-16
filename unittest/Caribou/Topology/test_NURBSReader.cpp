12014b84ce15f7534d6053d6e02caae555cf5aa5
Bhagath M <33441778+bhagath555@users.noreply.github.com>
2023-04-16 15:44:37 +0200

    [Unit-test] ADD good run of NURBSReader

--- unittest/Caribou/Topology/test_NURBSReader.cpp
+++ unittest/Caribou/Topology/test_NURBSReader.cpp
@@ -1,22 +1,30 @@
-#include "topology_test.h"
 #include <gtest/gtest.h>
 #include <Caribou/Topology/IO/NURBSReader.cpp>
+#include <string>
+#include "topology_test.h"
 
+using namespace caribou;
+using namespace caribou::topology;
+using namespace caribou::geometry;
 
 TEST(NURBSReader, knot_test_geo){
-    using namespace caribou;
-    using namespace caribou::topology;
-    using namespace caribou::geometry;
 
 //    using Patch = io::NURBSReader<_2D>::PatchType;
-    auto reader = io::NURBSReader<_2D>::Read(executable_directory_path + "/meshes/splines/xy_rectangle.txt");
+    std::string path = executable_directory_path + "/meshes/splines/knot_test_geo.txt";
+    auto reader = io::NURBSReader<_2D>::Read(path);
     auto patch = reader.patch();
-//    EXPECT_EQ(patch.number_of_nodes(), 15);
-    EXPECT_EQ(15, 15);
+    std::cout << "\n No. of nodes knot -> " << patch.number_of_nodes() << "\n";
+    EXPECT_EQ(patch.number_of_nodes(), 15);
+
 }
 
 TEST(SplinePatch, xy_rectangle){
-    EXPECT_EQ(2,2);
+//    using Patch = io::NURBSReader<_2D>::PatchType;
+    std::string path = executable_directory_path + "/meshes/splines/xy_rectangle.txt";
+    auto reader = io::NURBSReader<_2D>::Read(path);
+    auto patch = reader.patch();
+    std::cout << "\n No. of nodes xy -> " << patch.number_of_nodes() << "\n";
+    EXPECT_EQ(patch.number_of_nodes(), 9);
 }
 
 TEST(SplinePatch, quarter_cyl){
