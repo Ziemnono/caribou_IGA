e55b9c0050778a4f38c545ac8b73dd50994a2e32
Bhagath M <33441778+bhagath555@users.noreply.github.com>
2023-05-16 17:36:52 +0200

    [Topology] Add knot vectors as data members to spline patch.
    Along with updated class members, New class construction is implemented.

--- src/Caribou/Topology/SplinePatch.h
+++ src/Caribou/Topology/SplinePatch.h
@@ -5,7 +5,7 @@
 #include <Caribou/constants.h>
 #include <Caribou/Topology/BaseSplinePatch.h>
 #include <Caribou/Geometry/Element.h>
-#include <Caribou/Geometry/BezierSurf.h>
+#include <Caribou/Geometry/NurbsSurf.h>
 
 #include <Eigen/Dense>
 #include <memory>
@@ -16,14 +16,14 @@
 namespace caribou::topology {
 
 template<typename dtype>
-using Vector = Eigen::Matrix<dtype, 1, Eigen::Dynamic, Eigen::RowMajor>;
+using Vector = Eigen::Matrix<dtype, Eigen::Dynamic, 1>;
 
 template<typename dtype>
 using Matrix = Eigen::Matrix<dtype, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor>;
 
-using Double_Vector = Vector<double>;
+using Double_Vector = Vector<FLOATING_POINT_TYPE>;
 using Int_Vector = Vector<int>;
-using Double_Matrix = Matrix<double>;
+using Double_Matrix = Matrix<FLOATING_POINT_TYPE>;
 using Int_Matrix = Matrix<int>;
 using USInt_Matrix = Matrix<UNSIGNED_INTEGER_TYPE>;
 
@@ -91,14 +91,13 @@ public:
 
 //    using NodeIndex = UNSIGNED_INTEGER_TYPE;
 
-    using Element = geometry::BezierSurf<WorldDimension>;
+    using Element = geometry::NurbsSurf<WorldDimension>;
     //
     using ElementIndices = Eigen::Matrix<NodeIndex, geometry::traits<Element>::NumberOfNodesAtCompileTime, 1>;
     using ElementsIndices = Eigen::Matrix<NodeIndex, Eigen::Dynamic, geometry::traits<Element>::NumberOfNodesAtCompileTime, Eigen::RowMajor>;
     using ElementsKnotrange = Eigen::Matrix<FLOATING_POINT_TYPE, Eigen::Dynamic, geometry::traits<Element>::CanonicalDimension * 2, Eigen::RowMajor>; // 4 -> [u1, v1, u2, v2]
-    using ExtractionMatrix = Eigen::Matrix<FLOATING_POINT_TYPE, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor>;
-    using ElementsExtraction = std::vector<ExtractionMatrix>;
 
+    using DynVector = Eigen::Matrix<FLOATING_POINT_TYPE, Eigen::Dynamic, 1>;
 
     /**
      * Holder type that contains the nodes of the SplinePatch
@@ -107,7 +106,7 @@ public:
     /**
      * Eigen vector to store the node weights of the SplinePatch
      */
-    using WeightsContainer = Eigen::Matrix<FLOATING_POINT_TYPE, 1, Eigen::Dynamic, Eigen::RowMajor>;
+//    using DynVector = Eigen::Matrix<FLOATING_POINT_TYPE, Eigen::Dynamic, 1>;
 
     static_assert(WorldDimension == 1 or WorldDimension == 2 or WorldDimension == 3, "The world dimension must be 1, 2 or 3.");
 
@@ -119,7 +118,7 @@ public:
     /*!
      * Default constructor for an empty mesh.
      */
-    SplinePatch() : p_nodes {}, p_weights {}, p_indices{}, p_knotranges{}, p_extraction{} {}
+    SplinePatch() : p_nodes {}, p_weights {}, p_indices{}, p_knotranges{} {}
 
 //    /*!
 //     * Virtual default destructor
@@ -150,7 +149,7 @@ public:
         }
     }
 
-    explicit SplinePatch(const std::vector<WorldCoordinates> & positions, const WeightsContainer & weights)
+    explicit SplinePatch(const std::vector<WorldCoordinates> & positions, const DynVector & weights)
     {
         const auto n = positions.size();
 //        const auto n_weights = weights.size();
@@ -175,7 +174,7 @@ public:
      * @param positions A reference to a NxD matrix containing the position vector
      */
     template <typename Derived>
-    explicit SplinePatch(const Eigen::MatrixBase<Derived> & positions, const WeightsContainer & weights)
+    explicit SplinePatch(const Eigen::MatrixBase<Derived> & positions, const DynVector & weights)
     {
         static_assert(
             Eigen::MatrixBase<Derived>::ColsAtCompileTime == Dimension or Eigen::MatrixBase<Derived>::ColsAtCompileTime == Eigen::Dynamic,
@@ -200,15 +199,18 @@ public:
 
     // ================== Our specialization starts =========================
 
-    explicit SplinePatch(const NodeContainer_t & positions, const WeightsContainer & weights,
-                         const ElementsIndices & indices, const ElementsKnotrange & knotrange, const ElementsExtraction & extraction) :
-        p_nodes (positions), p_weights(weights), p_indices(indices), p_knotranges(knotrange), p_extraction(extraction)
-    { }
-
-
-    explicit SplinePatch(const std::vector<WorldCoordinates> & positions, const WeightsContainer & weights,
-                         const ElementsIndices & indices, const ElementsKnotrange & knotrange, const ElementsExtraction & extraction)
+    explicit SplinePatch(const NodeContainer_t & positions, const DynVector & weights,
+                         const ElementsIndices & indices, const DynVector & knot_1, const DynVector & knot_2, const ElementsKnotrange & knotrange) :
+        p_nodes(positions), p_weights(weights), p_indices(indices), p_knot_1(knot_1), p_knot_2(knot_2), p_knotranges(knotrange)
     {
+        std::cout << "\n1st constructor \n";
+    }
+
+
+    explicit SplinePatch(const std::vector<WorldCoordinates> & positions, const DynVector & weights,
+                         const ElementsIndices & indices, const ElementsKnotrange & knotrange)
+    {
+        std::cout << "\n2nd constructor \n";
         const int n = positions.size();
         const int n_weights = weights.size();
         if (n != n_weights){
@@ -225,12 +227,11 @@ public:
         }
         p_indices = indices;
         p_knotranges = knotrange;
-        p_extraction = extraction;
     }
 
     template <typename Derived>
-    explicit SplinePatch(const Eigen::MatrixBase<Derived> & positions, const WeightsContainer & weights,
-                         const ElementsIndices & indices, const ElementsKnotrange & knotrange, const ElementsExtraction & extraction)
+    explicit SplinePatch(const Eigen::MatrixBase<Derived> & positions, const DynVector & weights,
+                         const ElementsIndices & indices, const DynVector & knot_1, const DynVector & knot_2, const ElementsKnotrange & knotrange)
     {
         static_assert(
             Eigen::MatrixBase<Derived>::ColsAtCompileTime == Dimension or Eigen::MatrixBase<Derived>::ColsAtCompileTime == Eigen::Dynamic,
@@ -239,7 +240,6 @@ public:
 
         // The number of columns must equal the World dimension
         caribou_assert(positions.cols() == Dimension);
-
         // Do the copy
         const auto n = positions.rows();
         p_nodes.resize(n);
@@ -252,21 +252,23 @@ public:
             }
         }
         p_indices = indices;
+        p_knot_1 = knot_1;
+        p_knot_2 = knot_2;
         p_knotranges = knotrange;
-        p_extraction = extraction;
+
+
     }
 
     explicit SplinePatch(const Double_Matrix & positions, const Double_Vector & weights, const Matrix<NodeIndex> & indices,
-                         const Double_Matrix & knotrange, const std::vector<Double_Matrix> & extraction)
+                         const DynVector & knot_1, const DynVector & knot_2, const Double_Matrix & knotrange)
     {
-
-        // The number of columns must equal the World dimension
+        // The number of columns must equal the World dnimension
         caribou_assert(positions.cols() == Dimension);
 
         // Do the copy
         const auto n = positions.rows();
         p_nodes.resize(n);
-        p_weights.resize(n); // Resizing the p_weight vector
+        p_weights.resize(n);
         for (std::size_t i = 0; i < static_cast<std::size_t> (n); ++i) {
             auto node = this->p_nodes.node(i);
             p_weights(i) = weights(i); // Weight holder
@@ -275,14 +277,15 @@ public:
             }
         }
         p_indices = indices;
+        p_knot_1 = knot_1;
+        p_knot_2 = knot_2;
         p_knotranges = knotrange;
-        p_extraction = extraction;
     }
 
     explicit SplinePatch(const std::vector<WorldCoordinates> & positions, const Double_Vector & weights, const Matrix<NodeIndex> & indices,
-                         const Double_Matrix & knotrange, const std::vector<Double_Matrix> & extraction)
+                         const DynVector & knot_1, const DynVector & knot_2, const Double_Matrix & knotrange)
     {
-
+        std::cout << "\n5th constructor \n";
         // The number of columns must equal the World dimension
         caribou_assert(positions[0].size() == Dimension);
 
@@ -297,10 +300,10 @@ public:
                 node[j] = positions[i][j];
             }
         }
-        std::cout << "#### \n Evening specialisation \n #### \n";
         p_indices = indices;
+        p_knot_1 = knot_1;
+        p_knot_2 = knot_2;
         p_knotranges = knotrange;
-        p_extraction = extraction;
     }
 
     // ================== Our specialization ends ===========================
@@ -308,7 +311,8 @@ public:
 
     /*! Copy constructor */
     SplinePatch(const SplinePatch & other)
-    : p_nodes (other.p_nodes), p_weights (other.p_weights), p_indices(other.p_indices), p_knotranges(other.p_knotranges), p_extraction(other.p_extraction){ }
+    : p_nodes (other.p_nodes), p_weights (other.p_weights), p_indices(other.p_indices),
+      p_knot_1(other.p_knot_1), p_knot_2(other.p_knot_2), p_knotranges(other.p_knotranges){ }
 
     /*! Move constructor */
     SplinePatch(SplinePatch && other) noexcept
@@ -363,11 +367,26 @@ public:
         );
     }
 
+    void print_spline_patch(void){
+//        std::cout << "Control points : \n" << p_nodes  << "\n";
+        std::cout << "Weights \n" << p_weights.transpose() << "\n";
+        std::cout << "Indices \n" << p_indices << "\n";
+        std::cout << "Knot spans \n" << p_knotranges << "\n";
+        std::cout << "Knot U  : " << p_knot_1.transpose() << "\n";
+        std::cout << "Knot V  : " << p_knot_2.transpose() << "\n";
+
+    }
 
     inline auto element(const UNSIGNED_INTEGER_TYPE & element_id) const -> Element {
         caribou_assert(element_id < number_of_elements());
 
         using NodeMatrix = typename geometry::Element<Element>::template Matrix<geometry::traits<Element>::NumberOfNodesAtCompileTime, Dimension>;
+
+        DynVector knot1;
+        DynVector knot2;
+        DynVector weights(geometry::traits<Element>::NumberOfNodesAtCompileTime);
+        DynVector knot_span(geometry::traits<Element>::CanonicalDimension * 2);
+
         NodeMatrix node_positions;
         if constexpr (geometry::traits<Element>::NumberOfNodesAtCompileTime == caribou::Dynamic) {
             node_positions.resize(number_of_nodes_per_elements(), Dimension);
@@ -384,9 +403,12 @@ public:
             if constexpr(Dimension > 2) {
                 p1[2] = p2[2];
             }
+            weights[i] = p_weights[node_indices[i]];
         }
-
-        return Element(node_positions);
+        knot1 = this->p_knot_1;
+        knot2 = this->p_knot_2;
+        knot_span = this->p_knotranges.row(element_id);
+        return Element(node_positions, knot1, knot2, weights, knot_span);
     }
 
     inline auto element_knotranges(const UNSIGNED_INTEGER_TYPE & index) const {
@@ -397,11 +419,6 @@ public:
         );
     }
 
-
-    inline auto element_extraction(const UNSIGNED_INTEGER_TYPE & index) const {
-        caribou_assert(index < number_of_elements() and "Trying to get an element that does not exists.");
-        return p_extraction[index];
-    }
 
     // =============================== Domain helpers end ===========================
 
@@ -560,7 +577,7 @@ public:
      */
     template <typename IntegerType, std::size_t N>
     inline auto weights(const IntegerType(&indices)[N]) const {
-        Eigen::Matrix<FLOATING_POINT_TYPE, 1, N, Eigen::RowMajor> weights;
+        Eigen::Matrix<FLOATING_POINT_TYPE, N, 1> weights;
         for (std::size_t i = 0; i < N; ++i) {
             caribou_assert(indices[i] < static_cast<IntegerType>(number_of_nodes()));
             weights(i) = p_weights(indices[i]);
@@ -588,7 +605,7 @@ public:
     template <typename IntegerType>
     inline auto weights(const std::vector<IntegerType> & indices) const {
         const auto number_of_indices = indices.size();
-        Eigen::Matrix<FLOATING_POINT_TYPE, 1, Eigen::Dynamic, Eigen::RowMajor> weights;
+        Eigen::Matrix<FLOATING_POINT_TYPE, Eigen::Dynamic, 1> weights;
         weights.resize(number_of_indices);
         for (std::size_t i = 0; i < number_of_indices; ++i) {
             weights(i) = p_weights(indices[i]);
@@ -607,7 +624,7 @@ public:
         static_assert(EigenDerived::RowsAtCompileTime == 1 or EigenDerived::ColsAtCompileTime == 1,
             "Only vector type matrix can be used as the indices container.");
         const auto number_of_indices = indices.size();
-        Eigen::Matrix<FLOATING_POINT_TYPE, 1, Eigen::Dynamic, Eigen::RowMajor> weights;
+        Eigen::Matrix<FLOATING_POINT_TYPE, Eigen::Dynamic, 1> weights;
         weights.resize(number_of_indices);
         for (Eigen::Index i = 0; i < number_of_indices; ++i) {
             weights(i) = p_weights(indices.derived()[i]);
@@ -615,6 +632,9 @@ public:
         return weights;
     }
 
+    inline auto weights() const {
+        return p_weights;
+    }
     // ===================== Weights ===============================
 
     /*! Swap the data of two meshes */
@@ -626,8 +646,9 @@ public:
         swap(first.p_nodes, second.p_nodes);
         swap(first.p_weights, second.p_weights);
         swap(first.p_indices, second.p_indices);
+        swap(first.p_knot_1, second.p_knot_1);
+        swap(first.p_knot_2, second.p_knot_2);
         swap(first.p_knotranges, second.p_knotranges);
-        swap(first.p_extraction, second.p_extraction);
 
     }
 
@@ -641,16 +662,19 @@ private:
     /**
      * Container of the mesh node weights
      */
-    WeightsContainer p_weights;
+    DynVector p_weights;
 
-    /// stored outside of the domain instance), then this buffer is empty.
     ElementsIndices p_indices;
+
+    // Store U direction knot vector
+    DynVector p_knot_1;
+
+    // Store U direction knot vector
+    DynVector p_knot_2;
 
     /// knot_ranges containing the each elements knot range in both parametric directions
     ElementsKnotrange p_knotranges;
 
-    /// extraction containing the each elements extraction matrix
-    ElementsExtraction p_extraction;
 
 };
 } // namespace caribou::topology
