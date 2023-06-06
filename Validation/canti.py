3efee59ed0b590b0b3fb5b0aa8aa23b449a8d95e
Bhagath M <33441778+bhagath555@users.noreply.github.com>
2023-06-06 21:26:04 +0200

    [Validation] New example of Nurbs

---
+++ Validation/canti.py
@@ -0,0 +1,140 @@
+import Sofa
+import SofaCaribou
+import numpy as np
+#import Caribou.Geometry
+from Caribou.Topology import SplinePatch
+
+ELEMENT_TYPE = "Tetrahedron"
+ELEMENT_APPROXIMATION_DEGREE = 1
+MATERIAL_MODEL = "NeoHookean"
+FORCES = [0, -4000, 0]
+# TODO improve the manual permutation for matching the redefinition of the hexahedron
+
+nodes = [[0, 0], [0, 1], [0, 2],
+         [1, 0], [1, 1], [1, 2],
+         [2, 0], [2, 1], [2, 2]]
+
+knot1 = [0, 0, 0, 0.5, 1, 1, 1]
+knot2 = knot1
+weights = [1,1,1,1,1,1,1,1,1]
+knotspan = [0, 0, 1, 1]
+indices = [[0,1,2,3,4,5,6,7,8]]
+
+patch = SplinePatch(nodes, weights, indices, knot1, knot2, knotranges)
+print(patch);
+
+if MATERIAL_MODEL == "SaintVenantKirchhoff" or MATERIAL_MODEL == "NeoHookean":
+    material = MATERIAL_MODEL
+else:
+    raise ValueError('The material model is not implemented yet.')
+
+
+class ControlFrame(Sofa.Core.Controller):
+
+    def __init__(self, node):
+        Sofa.Core.Controller.__init__(self)
+        self.root = self.CreateGraph(node)
+
+    def CreateGraph(self, root):
+
+        root.addObject('DefaultVisualManagerLoop')
+        root.addObject('DefaultAnimationLoop')
+        root.addObject('VisualStyle', displayFlags="showForceFields showBehaviorModels")
+        root.addObject('RequiredPlugin',
+                       pluginName="SofaOpenglVisual SofaBaseMechanics SofaBaseTopology SofaSparseSolver SofaImplicitOdeSolver SofaTopologyMapping SofaBoundaryCondition SofaEngine")
+        root.gravity = [0, 0, 0]
+        sofa_node = root.addChild("sofa_node")
+        sofa_node.addObject('StaticSolver', newton_iterations="25", relative_correction_tolerance_threshold="1e-15",
+                            relative_residual_tolerance_threshold="1e-10", printLog="1")
+        sofa_node.addObject('SparseLDLSolver', template="CompressedRowSparseMatrixMat3x3d")
+        self.sofa_mo = sofa_node.addObject('MechanicalObject', name="mo", position=mesh.points.tolist())
+        sofa_node.addObject('CaribouSplineTopology', name='topo', template=element_sofa,
+                            indices=indices_sofa.tolist())
+
+#        sofa_node.addObject('BoxROI', name="fixed_roi", box="-7.5 -7.5 -0.9 7.5 7.5 0.1")
+#        sofa_node.addObject('FixedConstraint', indices="@fixed_roi.indices")
+#        sofa_node.addObject('BoxROI', name="top_roi", box="-7.5 -7.5 79.9 7.5 7.5 80.1")
+#        sofa_node.addObject('ConstantForceField', totalForce=FORCES, indices="@top_roi.indices")
+#        sofa_node.addObject(material + "Material", young_modulus="3000", poisson_ratio="0.3")
+#        sofa_node.addObject('HyperelasticForcefield', printLog=True)
+
+
+#        fenics_node = root.addChild("fenics_node")
+#        fenics_node.addObject('StaticSolver', newton_iterations="25", relative_correction_tolerance_threshold="1e-15",
+#                              relative_residual_tolerance_threshold="1e-10", printLog="1")
+#        fenics_node.addObject('SparseLDLSolver', template="CompressedRowSparseMatrixMat3x3d")
+#        self.fenics_mo = fenics_node.addObject('MechanicalObject', name="mo", position=mesh.points.tolist())
+#        fenics_node.addObject('CaribouTopology', name='topology', template=element_fenics,
+#                              indices=indices_fenics.tolist())
+#        fenics_node.addObject('BoxROI', name="fixed_roi", box="-7.5 -7.5 -0.9 7.5 7.5 0.1")
+#        fenics_node.addObject('FixedConstraint', indices="@fixed_roi.indices")
+#        fenics_node.addObject('BoxROI', name="top_roi", box="-7.5 -7.5 79.9 7.5 7.5 80.1")
+#        fenics_node.addObject('ConstantForceField', totalForce=FORCES, indices="@top_roi.indices")
+#        fenics_node.addObject('FEniCS_Material', template=element_fenics, young_modulus="3000",
+#                              poisson_ratio="0.3", C01=0.7, C10=-0.55, k=0.001, material_name=material, path="/home/..")
+
+#        fenics_node.addObject('HyperelasticForcefield_FEniCS', printLog=True)
+
+        return root
+
+#    def onSimulationInitDoneEvent(self, event):
+#        self.sofa_rest_position = np.array(self.sofa_mo.position.value.copy().tolist())
+#        self.fenics_rest_position = np.array(self.fenics_mo.position.value.copy().tolist())
+
+#    def onAnimateBeginEvent(self, event):
+
+#        pass
+
+#    def onAnimateEndEvent(self, event):
+#        sofa_current_positions = np.array(self.sofa_mo.position.value.copy().tolist())
+#        fenics_current_positions = np.array(self.fenics_mo.position.value.copy().tolist())
+
+#        errors = []
+#        for sofa_current_point, fenics_current_point, sofa_initial_point in zip(sofa_current_positions,
+#                                                                                fenics_current_positions,
+#                                                                                self.sofa_rest_position):
+#            if np.linalg.norm(sofa_current_point - sofa_initial_point) != 0:
+#                errors.append(np.linalg.norm(sofa_current_point - fenics_current_point) / np.linalg.norm(
+#                    sofa_current_point - sofa_initial_point))
+
+#        mean_error = np.mean(np.array(errors))
+
+#        displacement = fenics_current_positions - self.sofa_rest_position
+#        print(displacement[:, 1].min())
+
+#        print(f"Relative Mean Error: {100 * mean_error} %")
+
+
+def createScene(node):
+    node.addObject(ControlFrame(node))
+
+
+# Choose in your script to activate or not the GUI
+USE_GUI = True
+
+
+def main():
+    import SofaRuntime
+    import Sofa.Gui
+    SofaRuntime.importPlugin("SofaOpenglVisual")
+    SofaRuntime.importPlugin("SofaImplicitOdeSolver")
+    SofaRuntime.importPlugin("SofaLoader")
+
+    root = Sofa.Core.Node("root")
+    createScene(root)
+    Sofa.Simulation.init(root)
+
+    if not USE_GUI:
+        for iteration in range(10):
+            Sofa.Simulation.animate(root, root.dt.value)
+    else:
+        Sofa.Gui.GUIManager.Init("myscene", "qglviewer")
+        Sofa.Gui.GUIManager.createGUI(root, __file__)
+        Sofa.Gui.GUIManager.SetDimension(1080, 1080)
+        Sofa.Gui.GUIManager.MainLoop(root)
+        Sofa.Gui.GUIManager.closeGUI()
+
+
+# Function used only if this script is called from a python environment
+if __name__ == '__main__':
+    main()
